a62612e0dfd1c64d2b4de2658dad146be912f4d8
Rahi Ahmed <108581106+rahi45@users.noreply.github.com>
2022-11-07 08:39:01 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//finding main div perent of the input elements and button
+const div = document.querySelector("#inputs");
+
+//find the celsius input
+const cels = div.querySelector("#cels");
+
+//find the fahrenheit input
+const farn = div.querySelector("#farn");
+
+
+cels.addEventListener("input", celsHandler);
+
+function celsHandler() {
+    let output = parseFloat(cels.value * (9/5) +32 );
+
+    farn.value = parseFloat(output.toFixed(2));
+}
+
+
+farn.addEventListener("input", farnHandler);
+
+function farnHandler() {
+    let output = parseFloat(farn.value -32 * (5/9));
+
+    cels.value = parseFloat(output.toFixed(2));
+}
